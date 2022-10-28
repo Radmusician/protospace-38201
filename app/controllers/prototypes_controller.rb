68e576aee2cbc2328f4f6b2c0dc4c1527e3028b8
Radmusician <mcz5ny54gwt5mvb4ua1i@gmail.com>
2022-10-29 00:34:16 +0900

    eighth commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -31,7 +31,7 @@ class PrototypesController < ApplicationController
   end
 
   def update
-    prototype = Prototype.find(params[:id])
+    @prototype = Prototype.find(params[:id])
     if prototype.update(prototype_params)
       redirect_to prototype_path
     else
--- db/migrate/20221024142613_add_prototype_id_to_comments.rb
+++ db/migrate/20221024142613_add_prototype_id_to_comments.rb
@@ -1,5 +1,5 @@
 class AddPrototypeIdToComments < ActiveRecord::Migration[6.0]
   def change
-    add_reference :comments, :prototype, null: false
+    #add_reference :comments, :prototype, null: false
     end    
 end
